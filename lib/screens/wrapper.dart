f054f2a6f4cb042ae16e28e7a73b9f6b9da3f162
kenmusembi <kmusembi@mhealthkenya.org>
2021-01-12 08:49:16 +0300

    initial login page design

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:medyq_patient/screens/authenticate/welcome_page.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WelcomePage();
+  }
+}
